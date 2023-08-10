9c0e7fcc90af5f5dbd898dbfd8e13677a41571fd
Your Name <you@example.com>
2023-08-10 11:23:44 +0700

    дз по 6 пункт

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let a = 10
+alert( a );
+
+let iPhoneBirth = 2007
+alert( iPhoneBirth );
+
+let parentJs = "Brendan Eich"
+alert( parentJs );
+
+let b = 2
+let sum = a + b;
+alert( sum );
+let residual = a - b;
+alert(residual);
+let generation = a * b;
+alert(generation);
+let quotient = a / b;
+alert(quotient);
+
+let result = b * b * b * b * b;
+alert(result);
+
