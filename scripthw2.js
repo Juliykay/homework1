a2a9fe45dbe93b9f6cd41700b50830507ffef2fa
Your Name <you@example.com>
2023-08-10 16:18:04 +0700

    все задания по js

--- scripthw2.js
+++ scripthw2.js
@@ -1,6 +1,8 @@
 
-let a = 10
+let a = 10 
 alert( a );
+a = 20 
+alert(a);
 
 let iPhoneBirth = 2007
 alert( iPhoneBirth );
@@ -9,6 +11,7 @@ let parentJs = "Brendan Eich"
 alert( parentJs );
 
 let b = 2
+a = 10
 let sum = a + b;
 alert( sum );
 let residual = a - b;
@@ -18,6 +21,40 @@ alert(generation);
 let quotient = a / b;
 alert(quotient);
 
-let result = b * b * b * b * b;
+let result = b**5;
 alert(result);
 
+a = 9
+let res = a % b
+alert(res)
+
+let nam = 1;
+nam += 5;
+nam -= 3;
+nam *= 7;
+nam /= 3;
+++nam;
+--nam;
+alert(nam);
+
+
+
+
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+    name: "Ilya",
+    age: 25,
+    isAdmin: true,
+}
+
+user["city of residence"] = true;
+delete user["city of residence"];
+let info = prompt(`Какую информацию хотите узнать о пользователе? ${name}`);
+alert(user[info]);
+console.log(user[info]);
+
+let acquaintance = prompt('Как вас зовут?');
+alert(`hello ${acquaintance}`);
