8aa51bd67204b1d94a72df451df79fcd71b3b77d
Your Name <you@example.com>
2023-09-17 17:59:53 +0700

    дз № 4

---
+++ scripthw4.js
@@ -0,0 +1,53 @@
+/*
+let i = 1;
+ while ( i<3) {
+    console.log (`привет`);
+    i ++;
+ }*/
+
+
+
+
+/*let i = 1;
+ while (i < 6) {
+    console.log (i);
+    i ++;
+ }
+
+ let n = 7;
+ while (n < 23) {
+   console.log (n);
+   n ++;
+ }*/
+
+ /*let obj = {
+   Коля: `200`,
+   Вася: `300`,
+   Петя: `400`
+ };
+
+for (let key in obj) {
+    alert(`${key} - зарплата ${obj[key]}`);
+}*/
+
+
+
+
+  /* let a = 1000, num = 0;
+   while(a >= 50){
+    a = a/2;
+   num++;
+}
+console.log("Итераций: " + num +", получилось число " + a);
+*/
+let n = 1;
+while (n <= 31) {
+  console.log (n);
+  n ++;
+
+
+   if (n % 7 ===0)
+   alert(`Сегодня пятница ,необходимо подготовить отчет.`)
+}
+
+
